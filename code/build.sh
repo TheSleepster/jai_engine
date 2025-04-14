2d9f8191e2ec0a784285d32e23db9a9b518c7935
Sleepster <ibjal20@outlook.com>
2025-04-14 16:02:26 -0500

    read head

--- code/build.sh
+++ code/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 pushd ../build
-../../../.jai/bin/jai-linux ../code/main.jai -llvm -natvis -very_debug - no_inline -output_path../../build/
+# ../../../.jai/bin/jai-linux ../code/main.jai -llvm -natvis -very_debug - no_inline -output_path../../build/
+../../../.jai/bin/jai-linux ../code/main.jai -natvis -release
 
 # Check for compilation success
 if [ $? -ne 0 ]; then
