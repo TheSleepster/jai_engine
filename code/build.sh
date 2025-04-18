b3c58dcece9b9ea2c90fd552bf2b56a2ba748379
justin <jal4dsox@live.com>
2025-04-18 11:44:17 -0500

    AC-3 fixed (hopefully)... Right now it is faster than AC-1 in some instances. I may want to improve it by adding stuff like backtracking but for now I really just don't fucking care about that since it works and that means I can move on to more important things...

--- code/build.sh
+++ code/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pushd ../build
-~/.jai/bin/jai-linux ../code/main.jai -llvm -natvis -very_debug - no_inline -output_path../../build/
+~/.jai/bin/jai-linux ../code/main.jai -llvm -natvis -very_debug -no_inline -output_path ../build/ -exe game_DEBUG.out
 #~/.jai/bin/jai-linux ../code/main.jai -natvis -release
 
 # Check for compilation success
