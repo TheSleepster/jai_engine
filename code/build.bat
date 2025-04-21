c32a7dccf7e6adfa7933feeb842cddc5c1b9cce2
TheSleepster <jal4dsox@live.com>
2025-04-21 06:48:40 -0500

    sometimes the most complicated problems have the simplest solution... Changed the way WFC_generate_AC-3 works, now no longer calls itself recursively

--- code/build.bat
+++ code/build.bat
@@ -2,7 +2,7 @@
 
 IF NOT EXIST ..\build\ mkdir ..\build
 
-jai ../code/main.jai  -llvm -natvis  -debugger -very_debug -no_inline -no_backtrace_on_crash -output_path ../build/ -exe game_DEBUG
+jai ../code/main.jai  -llvm -natvis -debugger -very_debug -no_inline -output_path ../build/ -exe game_DEBUG
 REM jai ../code/main.jai -natvis -release -output_path ../build/ -exe game_DEBUG.exe
 
 @echo ====================
