7d477259899ceb2a04045dd361dabd9e2fe58d6c
TheSleepster <jal4dsox@live.com>
2025-04-20 20:04:38 -0500

    Investigating WFC_generate_AC-3, there's definitely something wrong with it

--- code/build.bat
+++ code/build.bat
@@ -2,13 +2,8 @@
 
 IF NOT EXIST ..\build\ mkdir ..\build
 
-jai ../code/main.jai -natvis -very_debug -no_inline
-REM jai ../code/main.jai -natvis -release
-copy *.exe ..\build\
-copy *.pdb ..\build\
-
-del *.exe
-del *.pdb
+jai ../code/main.jai  -llvm -natvis  -debugger -very_debug -no_inline -no_backtrace_on_crash -output_path ../build/ -exe game_DEBUG
+REM jai ../code/main.jai -natvis -release -output_path ../build/ -exe game_DEBUG.exe
 
 @echo ====================
 @echo Compilation Complete
