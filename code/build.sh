ae6c2586afed36fc1b5ebfde028cabc2ee1a11b3
TheSleepster <jal4dsox@live.com>
2025-04-22 06:30:42 -0500

    Lots of changes and improvements. Some TODO's added. AC-3 improved, should no longer hit the assert in the enqueue function since it's a darray now. Module generation should be improved.

--- code/build.sh
+++ code/build.sh
@@ -3,18 +3,3 @@
 pushd ../build
 ~/.jai/bin/jai-linux ../code/main.jai -llvm -natvis -very_debug -no_inline -output_path ../build/ -exe game_DEBUG.out
 #~/.jai/bin/jai-linux ../code/main.jai -natvis -release -output_path ../build/ -exe game_DEBUG.out
-
-# Check for compilation success
-if [ $? -ne 0 ]; then
-    echo "==================="
-    echo "Compilation Failure"
-    echo "==================="
-    exit 1
-fi
-
-# Return to the original directory
-popd || exit 1
-
-echo "===================="
-echo "Compilation Complete"
-echo "===================="
