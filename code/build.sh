4b1a214496f8a326943175a4afdf0db8a4ed788c
the_Sleepster <ibjal20@outlok.com>
2025-05-03 08:56:02 +0000

    FROM THE LAPTOP: finished adding main rooms to the grid. Other lesser rooms and hallways still need their info added

--- code/build.sh
+++ code/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pushd ../build
-~/.jai/bin/jai-linux ../code/main.jai -llvm -natvis -very_debug -no_inline -output_path ../build/ -exe game_DEBUG.out
+~/.jai/bin/jai-linux ../code/main.jai -llvm -natvis -very_debug -no_inline -no_color -output_path ../build/ -exe game_DEBUG.out
 #~/.jai/bin/jai-linux ../code/main.jai -natvis -release -output_path ../build/ -exe game_DEBUG.out
