b74c3939c9c13674c06704c983eae7874e9debb8
TheSleepster <jal4dsox@live.com>
2025-05-17 11:20:33 -0500

    multithreading cleanup

--- code/build.bat
+++ code/build.bat
@@ -3,4 +3,4 @@
 IF NOT EXIST ..\build\ mkdir ..\build
 
 jai ../code/main.jai  -llvm -natvis -debugger -very_debug -no_inline -output_path ../build/ -exe game_DEBUG
-REM jai ../code/main.jai -natvis -release -output_path ../build/ -exe game_DEBUG
+REM jai ../code/main.jai -llvm -natvis -release -output_path ../build/ -exe game_DEBUG
