a1b0eed29575ee2057cc8ded24e6f4e38970faea
the_Sleepster <ibjal20@outlok.com>
2025-05-19 06:43:40 +0000

    linux build.jai isn't working, need to recompile SDL

--- code/build.sh
+++ code/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-pushd ../build
-~/.jai/bin/jai-linux ../code/main.jai -llvm -natvis -very_debug -no_inline -no_color -output_path ../build/ -exe game_DEBUG.out
+#~/.jai/bin/jai-linux ../code/main.jai -llvm -natvis -very_debug -no_inline -no_color -output_path ../build/ -exe game_DEBUG.out
 #~/.jai/bin/jai-linux ../code/main.jai -natvis -release -output_path ../build/ -exe game_DEBUG.out
+
+~/.jai/bin/jai-linux build.jai -release 
